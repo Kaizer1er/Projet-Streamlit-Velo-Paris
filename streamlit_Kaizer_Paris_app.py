825126ec6fa39d26be59cd47148a14c65ae254da
Kaizer1er <160596503+Kaizer1er@users.noreply.github.com>
2024-07-28 01:36:02 +0200

    Update streamlit_Kaizer_Paris_app.py

--- streamlit_Kaizer_Paris_app.py
+++ streamlit_Kaizer_Paris_app.py
@@ -23,34 +23,6 @@ st.markdown("""
     padding-bottom: 0rem;
     margin-bottom: -7rem;
 }
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    padding: 11px 0;
-}
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 0%;
-    -webkit-transform: translateX(0%);
-    -ms-transform: translateX(0%);
-    transform: translateX(0%);
-}
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 0%;
-    -webkit-transform: translateX(0%);
-    -ms-transform: translateX(0%);
-    transform: translateX(0%);
-}
 </style>
 """, unsafe_allow_html=True)
 
